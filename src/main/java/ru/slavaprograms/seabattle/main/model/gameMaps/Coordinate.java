1feb7ed21c89863019ed8507bb39868948383879
aspire240688 <yakovv240688@gmail.com>
2024-12-08 13:11:43 +0300

    Добавлен общий метод получения координат методом ввода строки

--- src/main/java/ru/slavaprograms/seabattle/main/model/gameMaps/Coordinate.java
+++ src/main/java/ru/slavaprograms/seabattle/main/model/gameMaps/Coordinate.java
@@ -1,5 +1,9 @@
 package ru.slavaprograms.seabattle.main.model.gameMaps;
 
+import ru.slavaprograms.seabattle.main.model.game.ConsoleMenu;
+
+import java.util.Scanner;
+
 public class Coordinate {
     private final int horizontalCoordinate;
     private final int verticalCoordinate;
@@ -15,5 +19,21 @@ public class Coordinate {
 
     public int getVerticalCoordinate() {
         return verticalCoordinate;
+    }
+
+    /**
+     * Возвращает введенные координаты
+     * @return Координаты
+     */
+    public static Coordinate enterCoordinate(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите координаты горизонтали, вертикали " +
+                "через запятую например 0,1: ");
+        String coordinates = in.nextLine();
+        String[] crds = coordinates.split(",");
+        int horizontalCrd = Integer.parseInt(crds[0]);
+        int verticalCrd = Integer.parseInt(crds[1]);
+
+        return new Coordinate(horizontalCrd, verticalCrd);
     }
 }
