e6918a04943b2abe4157030d8c701334e76ea90a
Lenovo I7 <vitalikyakovlev@mail.ru>
2024-12-30 18:36:54 +0300

    Убран мусор

--- src/main/java/ru/slavaprograms/seabattle/main/Main.java
+++ src/main/java/ru/slavaprograms/seabattle/main/Main.java
@@ -2,8 +2,6 @@ package ru.slavaprograms.seabattle.main;
 
 import ru.slavaprograms.seabattle.main.model.game.ConsoleMenu;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         ConsoleMenu consoleMenu = new ConsoleMenu();
